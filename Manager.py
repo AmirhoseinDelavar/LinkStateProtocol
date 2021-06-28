14e3e6b59a2ca5a7603da9074e26bb9dbc7a810d
unknown <a.delavar@hamsaa.ir>
2021-06-28 15:23:21 +0430

    diksstara added and fixed

--- Manager.py
+++ Manager.py
@@ -1,3 +1,4 @@
+import heapq
 import os
 import sys
 import time
@@ -19,6 +20,48 @@ routers_safe_l = threading.Lock()
 routers_acked = 0
 routers_safe = 0
 log_f = 0
+man_order = {}
+
+
+def dijkstra(graph, src, dest, visited, distances, predecessors):
+    """ calculates a shortest path tree routed in src
+    """
+    # ending condition
+    if src == dest:
+        # We build the shortest path and display it
+        path = []
+        pred = dest
+        while pred != None:
+            path.append(pred)
+            pred = predecessors.get(pred, None)
+        # reverses the array, to display the path nicely
+        path.pop()
+        path.reverse()
+        return path
+
+    else:
+        # if it is the initial  run, initializes the cost
+        if not visited:
+            distances[src] = 0
+        # visit the neighbors
+        for neighbor in graph[src]:
+            if neighbor not in visited:
+                new_distance = distances[src] + graph[src][neighbor]
+                if new_distance < distances.get(neighbor, float('inf')):
+                    distances[neighbor] = new_distance
+                    predecessors[neighbor] = src
+        # mark as visited
+        visited.append(src)
+        # now that all neighbors have been visited: recurse
+        # select the non visited node with lowest distance 'x'
+        # run Dijskstra with src='x'
+        unvisited = {}
+        for k in graph:
+            if k not in visited:
+                unvisited[k] = distances.get(k, float('inf'))
+        x = min(unvisited, key=unvisited.get)
+        return dijkstra(graph, x, dest, visited, distances, predecessors)
+
 
 def router(id):
     udp_ip = '127.0.0.' + str(id + 2)
@@ -30,12 +73,9 @@ def router(id):
     socs = []
     # distance vector for topology
     d_dic = {}
-    d = []
-    # visited routers from LSP
-    visited_s = [0 for i in range(routers_cnt)]
-    visited_all = False
     # forwarding table
-    ft = []
+    ft = {}
+
     f = open('router{0}.txt'.format(id), 'a')
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
@@ -63,14 +103,14 @@ def router(id):
         f.write('Status From Manager: ' + repr(str(status)) + '\n')
         f.flush()
         # setup udp connections
-        for ip,port in zip(n_udp_ip,n_udp_port):
+        for ip, port in zip(n_udp_ip, n_udp_port):
             soc_t = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
             # Listen for incoming datagrams
-            soc_t.sendto(str.encode('ack from {0} to {1}'.format(id,PORTS_START-port-1)), (ip, port))
+            soc_t.sendto(str.encode('ack from {0} to {1}'.format(id, PORTS_START - port - 1)), (ip, port))
             socs.append(soc_t)
 
         for port in n_udp_port:
-            f.write(str(soc.recvfrom(1024)[0])+'\n')
+            f.write(str(soc.recvfrom(1024)[0]) + '\n')
             f.flush()
         # tell manager all neighbors are ok
         s.sendall(b'Acked')
@@ -80,28 +120,89 @@ def router(id):
 
         # send LSP to all neighbors
         d_dic[id] = list(neighbors)
-        for soc_t,ip,port in zip(socs,n_udp_ip,n_udp_port):
+        for soc_t, ip, port in zip(socs, n_udp_ip, n_udp_port):
             t_list = neighbors.copy()
             t_list.append(id)
             soc_t.sendto(bytes(t_list), (ip, port))
         f.write('Send LSP To Neighbors\n')
         f.flush()
+        # receive and resend LSP to all neighbors
         while True:
             resp = list(soc.recvfrom(1024)[0])
             neigh_id = resp.pop()
             if neigh_id not in d_dic.keys():
                 d_dic[neigh_id] = resp
             resp.append(neigh_id)
-            for soc_t,ip,port in zip(socs,n_udp_ip,n_udp_port):
+            for soc_t, ip, port in zip(socs, n_udp_ip, n_udp_port):
                 soc_t.sendto(bytes(resp), (ip, port))
             if len(d_dic.items()) == routers_cnt:
                 break
+        # log network topology
         f.write(str(d_dic) + '\n')
         f.flush()
-        # receive and resend LSP to all neighbors
+        # build graph
+        graph = {}
+        for key in d_dic.keys():
+            graph[key] = {}
+            for index, val in enumerate(d_dic.get(key)):
+                if index >= routers_cnt:
+                    break
+                if MAX_COST > val > 0:
+                    graph[key][index] = val
+        # log graph network topology
+        f.write(str(graph) + '\n')
+        f.flush()
+        # create SPT
+        for i in range(routers_cnt):
+            if i == id:
+                ft[id] = [id]
+            else:
+                ft[i] = dijkstra(graph, id, i,[],{},{})
+
+        f.write(str(ft))
+        f.flush()
+        # save SPT
+        f_spt = open('routerSPT{0}.txt'.format(id), 'w')
+        f_spt.write(str(ft))
+        f_spt.flush()
+        f_spt.close()
+        # resend received packet
+        # send or quit order from manager
+        while True:
+            order = str(s.recv(1024))
+            if order == b'quit':
+                break
+            else:
+                dest_id = int(order)
+                next_id = ft[dest_id][0]
+                next_port = PORTS_START - next_id - 1
+                next_index = n_udp_port.index(next_port)
+                packet = ["data", dest_id, id]
+                socs[next_index].sendto(bytes(packet), (n_udp_ip[next_index], next_port))
+                f.write('Start Transmission Packet ' + str(id) + ' to ' + str(dest_id) + '\n')
+                f.flush()
+
+            packet = list(soc.recvfrom(1024)[0])
+            if packet[1] == id:
+                f.write('Packet rec from' + str(packet[2]) + ' ' + '\n')
+                f.flush()
+            else:
+                next_id = ft[packet[1]][0]
+                next_port = PORTS_START - next_id - 1
+                next_index = n_udp_port.index(next_port)
+                socs[next_index].sendto(bytes(packet), (n_udp_ip[next_index], next_port))
+                f.write('Transit Packet ' + str(packet[2]) + ' to ' + str(packet[1]) + '\n')
+                f.flush()
+
+        # closing thread
+        soc.close()
+        s.close()
+        f.close()
+
 
     except Exception as e:
         print(e.with_traceback())
+
 
 def manager_tcp(i):
     global routers_acked
@@ -109,6 +210,7 @@ def manager_tcp(i):
     global routers_acked_l
     global routers_safe
     global routers_safe_l
+    global man_order
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('127.0.0.1', PORTS_START + i))
     s.listen()
@@ -146,7 +248,18 @@ def manager_tcp(i):
         log_f.write('Ready to Route Sent for R {0}\n'.format(i))
         log_f.flush()
 
-        #
+        # send order to dest
+        while i not in man_order.keys():
+            pass
+        print("here")
+        conn.sendall(bytes(man_order[i], encoding='UTF-8'))
+        log_f.write('Send Order R {0}\n msg {1}'.format(i, man_order[i]))
+        log_f.flush()
+        time.sleep(0.001)
+        # send quit
+        conn.sendall(bytes(man_order[i], encoding='UTF-8'))
+        log_f.write('Send Order R {0}\n msg {1}'.format(i, man_order[i]))
+        log_f.flush()
     except Exception as e:
         print(e.with_traceback())
 
@@ -156,6 +269,7 @@ def main():
     global routers_acked
     global routers_cnt
     global routers_safe
+    global man_order
     # keeping distances
     d = []
     # routers ip and ports
@@ -181,8 +295,6 @@ def main():
             d[int(inputs[0])][int(inputs[1])] = int(inputs[2])
             d[int(inputs[1])][int(inputs[0])] = int(inputs[2])
 
-    # print read file
-    # print(d)
     # save log for reading config
     log_f.write('Read Config\n')
     log_f.flush()
@@ -191,7 +303,6 @@ def main():
     manager_tcp_shared = [d[i] for i in range(routers_cnt)]
     # instantiate routers
     for i in range(routers_cnt):
-        # print('Router' + str(i))
         # save log for creating routers
         log_f.write('Created Router {0}\n'.format(i))
         log_f.flush()
@@ -203,7 +314,6 @@ def main():
         tcp_ports.append(str(PORTS_START + i))
     # connect with tcp to routers
     for i in range(routers_cnt):
-        # print('TCP Manager' + str(i))
         newt = threading.Thread(target=manager_tcp, args=(i,), daemon=True)
         newt.start()
         tcp_s.append(newt)
@@ -212,13 +322,22 @@ def main():
     with open('orders.txt') as f_orders:
         for line in f_orders.readlines():
             inputs = line.strip().split(sep=' ')
-            orders.append([inputs[0],inputs[1]])
-    # print(orders)
+            orders.append([int(inputs[0]), int(inputs[1])])
+
+
+    # send relative packets to routers
+    for order in orders:
+        man_order[order[0]] = order[1]
+    print(man_order)
+    time.sleep(0.005)
+    # send quit order
+    for order in orders:
+        man_order[order[0]] = 'quit'
+    time.sleep(0.005)
 
     while True:
         pass
-    # send relative packets to routers
 
-    # send quit order
+
 if __name__ == '__main__':
     main()
